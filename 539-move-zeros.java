60dca4e5824355442c053e8a2cbfd53a43d20427
lixuanqi <lixuanqi1995@gmail.com>
2018-04-04 10:13:17 +0800

    539

---
+++ 539-move-zeros.java
@@ -0,0 +1,19 @@
+public class Solution {
+    /**
+     * @param nums: an integer array
+     * @return: nothing
+     */
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != 0) {
+                int temp = nums[fast];
+                nums[fast] = nums[slow];
+                nums[slow] = temp;
+                slow++;
+            }
+            fast++;
+        }
+    }
+}
