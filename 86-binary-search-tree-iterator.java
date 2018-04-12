3ad9d8115eab8a05de8b5615efa7102badb2db56
lixuanqi <lixuanqi1995@gmail.com>
2018-04-12 21:44:58 +0800

    86

---
+++ 86-binary-search-tree-iterator.java
@@ -0,0 +1,64 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Example of iterate a tree:
+ * BSTIterator iterator = new BSTIterator(root);
+ * while (iterator.hasNext()) {
+ *    TreeNode node = iterator.next();
+ *    do something for node
+ * } 
+ */
+
+
+public class BSTIterator {
+    
+    private Stack<TreeNode> stack = new Stack<>();
+    /*
+    * @param root: The root of binary tree.
+    */public BSTIterator(TreeNode root) {
+        // do intialization if necessary
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+
+    /*
+     * @return: True if there has next node, or false
+     */
+    public boolean hasNext() {
+        // write your code here
+        return !stack.isEmpty();
+    }
+
+    /*
+     * @return: return next node
+     */
+    public TreeNode next() {
+        // write your code here
+        TreeNode node = stack.peek();
+        TreeNode result = node;
+        if (node.right == null) {
+            node = stack.pop();
+            while (!stack.isEmpty() && stack.peek().right == node) {
+                node = stack.pop();
+            }
+        } else {
+            node = node.right;
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+        return result;
+    }
+}
+
+// 1-10-11
