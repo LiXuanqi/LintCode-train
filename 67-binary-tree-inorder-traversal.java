92e7f2b5da26cd3fa66689d681c56b471fc35d3f
lixuanqi <lixuanqi1995@gmail.com>
2018-04-12 21:34:51 +0800

    67

---
+++ 67-binary-tree-inorder-traversal.java
@@ -0,0 +1,47 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // write your code here
+        Stack<TreeNode> stack = new Stack<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+        
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.peek();
+            result.add(node.val);
+            
+            if (node.right == null) {
+                node = stack.pop();
+                while (!stack.isEmpty() && stack.peek().right == node) {
+                    node = stack.pop();
+                }
+            } else {
+                node = node.right;
+                while (node != null) {
+                    stack.push(node);
+                    node = node.left;
+                }
+            }
+        }
+        return result;
+    }
+}
